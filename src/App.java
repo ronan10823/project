2c5c7216760a49faade820dcb2e5f402b7542778
BaekSeungCheol <kyn04241@gmail.com>
2025-10-22 13:16:04 +0900

    22222222

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("충돌 요망");
-        System.out.println("111111111111");
+        System.out.println("22222222222");
     }
 }
