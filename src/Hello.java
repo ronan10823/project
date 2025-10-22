6f9978c46da4edbd3ea3cad0ddc79d865a3f0e4e
ronan10823 <louck10823@gmail.com>
2025-10-22 13:21:14 +0900

    hello 출력문 추가

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("안녕하세요!");
         System.out.println("다들 건강하셨으면 좋겠네요.");
         System.out.println("끝까지 힘내봅시다.");
+        System.out.println("반갑습니다.");
     }
 }
