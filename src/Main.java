923d904e13b79f78fffcc86c782d1866b0d0d10f
ronan10823 <louck10823@gmail.com>
2025-10-22 13:16:39 +0900

    main 출력문 추가

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요!");
+        System.out.println("다들 건강하셨으면 좋겠네요.");
+        System.out.println("끝까지 힘내봅시다.");
+    }
+}
